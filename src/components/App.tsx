d44f5c2f7701c45a73fa2ec148adaf0a2aa6de32
KCM <knightedcodemonkey@gmail.com>
2026-03-28 19:49:59 -0500

    chore: sync editor component and styles from @knighted/develop

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import type { MouseEvent } from 'react'
+
+type CounterButtonProps = {
+  label: string
+  active: boolean
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void
+}
+
+const CounterButton = ({ label, active, onClick }: CounterButtonProps) => (
+  <button
+    type="button"
+    data-active={active ? "true" : "false"}
+    className={active ? "counter-button is-even" : "counter-button"}
+    onClick={onClick}
+  >
+    {label}
+  </button>
+)
+
