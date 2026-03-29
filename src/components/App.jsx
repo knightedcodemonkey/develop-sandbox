cc6614b526a9adae89bb01628595f4367931aeab
KCM <knightedcodemonkey@gmail.com>
2026-03-28 19:07:04 -0500

    chore: sync editor component and styles from @knighted/develop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,5 @@
-
-
-const CounterButton = ({ label, onClick }) => (
+const CounterButton = ({ label = 'default', onClick }) => (
   <button class="counter-button" type="button" onClick={onClick}>
     {label}
   </button>
 )
-
