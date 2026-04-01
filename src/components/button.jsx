09fff79e15a4b8653de901e41669ec80b85e30d5
KCM <knightedcodemonkey@gmail.com>
2026-03-31 20:28:33 -0500

    refactor: poo button with hannah.

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
-const Button = () => <button type="button" className="btn">tap me</button>
+const Button = ({ text = "💩" }) => {
+  const handleClick = (event) => {
+    const button = event.currentTarget;
+    button.classList.add('bounce');
+    const handleAnimationEnd = () => {
+      button.classList.remove('bounce');
+      button.removeEventListener('animationend', handleAnimationEnd);
+    };
+    button.addEventListener('animationend', handleAnimationEnd);
+  };
+
+  return <button type="button" className="btn" onClick={handleClick}>{text}</button>
+}
