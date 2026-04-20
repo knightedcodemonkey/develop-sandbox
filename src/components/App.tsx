d94ea4ca211d46e66c2c0b18bc12e8e0a4378946
KCM <knightedcodemonkey@gmail.com>
2026-04-19 21:13:18 -0500

    refactor: boop beep fix.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,1 @@
-import '../styles/app.css'
-import { Boop } from '../components/boop.js'
-import { Beep } from '../components/beep.js'
-
-const App = () => {
-  return (
-    <>
-      <Boop />
-      <Beep />
-    </>
-  )
-}
+export const Beep = () => <p>Beep Beep</p>
