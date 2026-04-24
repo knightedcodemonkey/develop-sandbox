4b83e36e1fea82a1ad1bf91fde7d10e13baeb595
KCM <knightedcodemonkey@gmail.com>
2026-04-23 21:41:36 -0500

    refactor: bop bop

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/styles.css'
 import { Boop } from './components/boop.js'
 import { Beep } from './components/beep.js'
 
-const Bop = () => <p>bop</p>
+const Bop = () => <p>bop bop</p>
 
 function App () {
   return (
