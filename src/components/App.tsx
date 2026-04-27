7326e02cc7baa5eb2427cd426c89edceaf109c1f
KCM <knightedcodemonkey@gmail.com>
2026-04-27 09:40:17 -0500

    chore: sync editor updates from @knighted/develop

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import { Button } from '../components/button.js'
+
+export const App = () => (
+  <Button />
+)
---
+++ src/components/button.tsx
@@ -0,0 +1,2 @@
+import '../styles/button.css'
+export const Button = () => <button>component-8e0n-b4oh</button>
