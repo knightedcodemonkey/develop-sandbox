e23b985dada10ec1ec023d5b00471aaf17ce3e87
KCM <knightedcodemonkey@gmail.com>
2026-04-30 13:10:44 -0500

    refactor: fish to dog.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import '../styles/app.scss'
 import { List } from '../components/list.js'
 import { Item } from '../components/item.js'
 
-const items = [1, 2, 3, 'fish']
+const items = [1, 2, 3, 'dog']
 
 const App = () => {
   return (
